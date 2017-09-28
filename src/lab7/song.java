d0fb319ced63db235d8cc6163997312b743fb18c
Brihi <brihi16142@iiitd.ac.in>
2017-09-28 14:55:08 +0530

    Created objects

---
+++ src/lab7/song.java
@@ -0,0 +1,15 @@
+package lab7;
+
+public class song {
+	
+	public String name;
+	public String singer;
+	public int duration;
+	
+	public song(String name, String singer, int duration) {
+		this.name = name;
+		this.singer = singer;
+		this.duration = duration;
+	}
+	
+}
